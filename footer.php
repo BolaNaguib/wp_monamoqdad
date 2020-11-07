fbe86c5b6ac3d03fc3cdcb2681a5f4f3416cbad2
Bola <bolanaguib@gmail.com>
2020-11-07 23:00:59 +0200

    [Added ] ig feed and acf footer

--- footer.php
+++ footer.php
@@ -1,3 +1,20 @@
+<?php
+// ACF FIELDS 
+$fb = get_field('facebook', 'option') ? get_field('facebook', 'option') : '#';
+$ig = get_field('instagram', 'option') ? get_field('instagram', 'option') : '#';
+$tw = get_field('twitter', 'option') ? get_field('twitter', 'option') : '#';
+$li = get_field('linkedin', 'option') ? get_field('linkedin', 'option') : '#';
+
+$col1_title = get_field('col1_title', 'option') ? get_field('col1_title', 'option') : 'About Us';
+$col1_caption = get_field('col1_caption', 'option') ? get_field('col1_caption', 'option') : 'Proin suscipit luctus orci placerat eat fringilla. Donec hendrerit laoreet risus volutpat mauris. Sed enim bibendum eu forest for ever';
+$col2_title = get_field('col2_title', 'option') ? get_field('col2_title', 'option') : 'Head Office';
+$email = get_field('email', 'option') ? get_field('email', 'option') : 'admin@admin.com';
+$location = get_field('location', 'option') ? get_field('location', 'option') : '154 Av the good, New York ';
+$phone = get_field('phone', 'option') ? get_field('phone', 'option') : '01234567890';
+$phone_display = get_field('phone_display', 'option') ? get_field('phone_display', 'option') : '01234567890';
+$col3_title = get_field('col3_title', 'option') ? get_field('col3_title', 'option') : 'Links';
+
+?>
 <!-- Footer -->
 <footer style="relative" class="bg-black text-gray-500 py-24 text-sm">
     <!-- START container -->
@@ -6,15 +23,15 @@
         <div class='grid grid-cols-1 lg:grid-cols-4 gap-6'>
             <!-- START footer card  -->
             <div>
-                <span class="pl-4 border-l-4 mb-4 block text-white font-semibold text-xl border-white">About Us</span>
+                <span class="pl-4 border-l-4 mb-4 block text-white font-semibold text-xl border-white"><?php echo $col1_title ?></span>
                 <p class="text-gray-600">
-                    Proin suscipit luctus orci placerat eat fringilla. Donec hendrerit laoreet risus volutpat mauris. Sed enim bibendum eu forest for ever
+                <?php echo $col1_caption ?>
                 </p>
             </div>
             <!-- END Footer Card  -->
             <!-- START footer card  -->
             <div>
-                <span class="pl-4 border-l-4 mb-4 block text-white font-semibold text-xl border-white">Head Office</span>
+                <span class="pl-4 border-l-4 mb-4 block text-white font-semibold text-xl border-white"><?php echo $col2_title ?></span>
                 <ul>
                     <li class="flex items-center mb-2">
                         <span class=" w-4 h-4 mr-2 inline-block">
@@ -23,7 +40,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                             </svg>
                         </span> <span>
-                            1422 1st St. Santa Rosa,t CA, USA.
+                        <?php echo $location ?>
                         </span>
                     </li>
                     <li class="flex items-center  mb-2">
@@ -31,14 +48,14 @@
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                             </svg>
-                        </span> <span>admin@e-mail.com </span>
+                        </span> <span><?php echo $email ?></span>
                     </li>
                     <li class="flex items-center  mb-2">
                         <span class=" w-4 h-4 mr-2 inline-block">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />
                             </svg>
-                        </span> <span>001- 01008431112 </span>
+                        </span> <span><a href="tel:<?php echo $phone ?>"><?php echo $phone_display ?></a></span>
                     </li>
                 </ul>
 
@@ -46,18 +63,19 @@
             <!-- END Footer Card  -->
             <!-- START footer card  -->
             <div>
-                <span class="pl-4 border-l-4 mb-4 block text-white font-semibold text-xl border-white">Links</span>
+                <span class="pl-4 border-l-4 mb-4 block text-white font-semibold text-xl border-white"><?php echo $col3_title ?></span>
                 <ul>
-                    <li class="flex items-center mb-2">
-                        <a class="text-current hover:text-white" href="#">Home</a>
-                    </li>
-                    <li class="flex items-center mb-2">
-                        <a class="text-current hover:text-white" href="#">Home</a>
-                    </li>
-                    <li class="flex items-center mb-2">
-                        <a class="text-current hover:text-white" href="#">Home</a>
-                    </li>
-
+                <?php if (have_rows('links', 'option')) : ?>
+                        <?php while (have_rows('links', 'option')) : the_row();
+                            //ACF Fields
+                            $page_link = get_sub_field('page_link') ? get_sub_field('page_link') : '#';
+                            $page_title = get_sub_field('page_title') ? get_sub_field('page_title') : 'Home';
+                        ?>
+                            <li class="flex items-center mb-2">
+                                <a class="text-current hover:text-white" href="<?php echo $page_link ?>"><?php echo $page_title ?></a>
+                            </li>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
                 </ul>
 
             </div>
@@ -65,7 +83,9 @@
             <!-- START footer card  -->
             <div>
                 <span class="pl-4 border-l-4 mb-4 block text-white font-semibold text-xl border-white">Instagram Feed</span>
-
+                <?php
+                    echo do_shortcode('[instagram-feed]');
+                    ?>  
 
             </div>
             <!-- END Footer Card  -->
@@ -81,23 +101,23 @@
             <a href="https://twitter.com/knyttneve" class="text-gray-500 ml-1" rel="noopener noreferrer" target="_blank">Inetwork-ME</a>
         </p>
         <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-            <a class="text-gray-600">
+            <a href='<?php echo $fb ?>' class="text-gray-600">
                 <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                     <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                 </svg>
             </a>
-            <a class="ml-3 text-gray-600">
+            <a href='<?php echo $tw ?>' class="ml-3 text-gray-600">
                 <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                     <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                 </svg>
             </a>
-            <a class="ml-3 text-gray-600">
+            <a href='<?php echo $ig ?>' class="ml-3 text-gray-600">
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                     <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                     <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                 </svg>
             </a>
-            <a class="ml-3 text-gray-600">
+            <a href='<?php echo $li ?>' class="ml-3 text-gray-600">
                 <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
                     <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
                     <circle cx="4" cy="4" r="2" stroke="none"></circle>
